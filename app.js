399114d4a1fd3a161f6bab5e8fc1b35efa176830
Ali Maklad <ali2812002@gmail.com>
2025-06-27 19:02:52 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,8 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const PORT = process.env.PORT || 3000;
-<<<<<<< HEAD
 const mqttHandler = require("./mqtthandler.js");
 mqttHandler();
-=======
-
->>>>>>> bc966a8f (Recent Updates)
 
 app.use(express.static(__dirname + "/Public"));
 app.get("/", (req, res) => {
@@ -24,62 +20,33 @@ app.get("/sender", (req, res) => {
 let counter = 1;
 
 io.on("connection", (socket) => {
-
   const userId = counter++;
   io.emit("connection", userId);
   console.log(`User ${userId} Connected`);
-<<<<<<< HEAD
-  
-  socket.on("Test", (testMessage) => {
-    console.log(testMessage);
-  });
-
-  socket.on("chat message", (msg) => {
-    io.emit("sendMsgToAllUsers", msg, userId);
-    console.log("User" + userId + ":", msg);
-  });
-
-  socket.on("myLocation", (currentLocation) => {
-    console.log(currentLocation);
-    io.emit("sendMyLocation", currentLocation);
-  });
-
-  socket.on("accident", (message, accidentLocation) => {
-    console.log("Accident Alert from app.js:", accidentLocation);
-    io.emit("sendAccidentAlert", message, accidentLocation);
-=======
 
   socket.on("userPayload", (Payload) => {
-    
-    if (Payload.intersection == false && Payload.accident == false){
-    console.log(Payload);
-
-    io.emit("sendUserLocation", Payload);
-    }
-    else if (Payload.intersection == true && Payload.accident == false) {
+    if (Payload.intersection == false && Payload.accident == false) {
+      console.log(Payload);
+      io.emit("sendUserLocation", Payload);
+    } else if (Payload.intersection == true && Payload.accident == false) {
       console.log("Intersection Alert from app.js:", Payload);
-    io.emit("intersectionDetected",  Payload);
+      io.emit("intersectionDetected", Payload);
     }
   });
 
   socket.on("accident", (accidentLocation) => {
     console.log("Accident Alert from app.js:", accidentLocation);
-    io.emit("accidentDetected",  accidentLocation);
-    
->>>>>>> bc966a8f (Recent Updates)
+    io.emit("accidentDetected", accidentLocation);
   });
 
   socket.on("highSpeed", (msg) => {
     io.emit("sendHighSpeedAlert", msg);
   });
-<<<<<<< HEAD
-=======
 
   socket.on("intersection", (car2Location) => {
     console.log("Intersection Alert from app.js:", car2Location);
-    io.emit("intersectionDetected",  car2Location);
+    io.emit("intersectionDetected", car2Location);
   });
->>>>>>> bc966a8f (Recent Updates)
 });
 
 server.listen(PORT, () => {
